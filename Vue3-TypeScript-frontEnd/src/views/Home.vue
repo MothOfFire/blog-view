5f8f54d814d47df3e794e76b5fa4fe28fd1f6b1b
MothOfFire <1748972220@qq.com>
2023-09-22 16:28:15 +0800

    feat:添加了一些工具包

--- Vue3-TypeScript-frontEnd/src/views/Home.vue
+++ Vue3-TypeScript-frontEnd/src/views/Home.vue
@@ -23,8 +23,9 @@
             <div class="home-body">
                 <!-- 跳转连接 -->
                 <div class="list">
-                    
+
                 </div>
+                <!-- 标语 -->
                 <div class="introduce"> 历历浮生，无非败而后成 </div>
             </div>
         </div>
@@ -32,5 +33,110 @@
 </template>
 
 <style lang="sass" scoped>
+.home {
+  position: fixed;
+  left: 0;
+  top: 0;
+  .content {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    font-size: 20px;
+    text-align: center;
+    padding-top: 12%;
+    .home-logo {
+      width: 220px;
+      border-radius: 50%;
+      animation: mylogo 3s;
+      -moz-animation: mylogo 3s; /* Firefox */
+      -webkit-animation: mylogo 3s; /* Safari and Chrome */
+      -o-animation: mylogo 3s; /* Opera */
+      animation-iteration-count: infinite;
+    }
+    .home-header {
+      .link {
+        display: block;
+      }
+    }
+    .home-body {
+      padding-top: 20px;
+      .list {
+        .link {
+          display: inline-block;
+          padding: 20px;
+          color: #409eff;
+          min-width: 80px;
+        }
+        .link:hover {
+          color: rgb(155, 35, 35);
+        }
+      }
+    }
+    .introduce {
+      padding-top: 20px;
+      color: #fff;
+    }
+  }
+}
+@keyframes mylogo {
+  0% {
+    transform: rotate(0deg) scale(0.8, 0.8);
+    opacity: 1;
+  }
+  25% {
+    transform: rotate(0deg) scale(1, 1);
+    opacity: 0.8;
+  }
+  100% {
+    transform: rotate(0deg) scale(0.8, 0.8);
+    opacity: 1;
+  }
+}
 
+@-moz-keyframes mylogo {
+  0% {
+    transform: rotate(0deg) scale(0.8, 0.8);
+    opacity: 1;
+  }
+  25% {
+    transform: rotate(0deg) scale(1, 1);
+    opacity: 0.8;
+  }
+  100% {
+    transform: rotate(0deg) scale(0.8, 0.8);
+    opacity: 1;
+  }
+}
+
+@-webkit-keyframes mylogo {
+  0% {
+    transform: rotate(0deg) scale(0.8, 0.8);
+    opacity: 1;
+  }
+  25% {
+    transform: rotate(0deg) scale(1, 1);
+    opacity: 0.8;
+  }
+  100% {
+    transform: rotate(0deg) scale(0.8, 0.8);
+    opacity: 1;
+  }
+}
+
+@-o-keyframes mylogo {
+  0% {
+    transform: rotate(0deg) scale(0.8, 0.8);
+    opacity: 1;
+  }
+  25% {
+    transform: rotate(0deg) scale(1, 1);
+    opacity: 0.8;
+  }
+  100% {
+    transform: rotate(0deg) scale(0.8, 0.8);
+    opacity: 1;
+  }
+}
 </style>
